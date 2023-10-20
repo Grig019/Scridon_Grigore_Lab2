7c2c3027af64c81ea8a3fc75e915f248436b3ad2
Grig <Grig@DESKTOP-UOHTR8R>
2023-10-20 17:07:23 +0300

    Add project files.

---
+++ Models/Books.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks; 
+
+namespace Scridon_Grigore_Lab2.Models
+
+{
+    
+    public class Books
+    { 
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+
+    }
+}
